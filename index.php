3251064f91519276afccf7b740d9a48b7e46cacc
SebastianH777 <160554431+SebastianH777@users.noreply.github.com>
2024-10-02 19:01:46 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -14,23 +14,50 @@ $enlace = mysqli_connect ($server,$usuario,$clave,$base_de_datos);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>formulario</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <form action="#" name="formulario" method = "post" >
-        <input type="text" name="nombre" placeholder="nombre">
-        <input type="text" name="telefono" placeholder="telefono">
-        <input type="email" name="correo" placeholder="correo">
-        <input type="text" name="experiencia_laboral" placeholder="experiencia_laboral">
-        <input type="text" name="formacion" placeholder="formacion">
-        <input type="text" name="idiomas" placeholder="idiomas">
-        <input type="text" name="habilidades" placeholder="habilidades">
+    <div class="container">
+        <header class="header">
+            <div class="title">
+            <input type="text" name="nombre" placeholder="nombre">
+                <h1>Curriculum Vitae</h1>
+                <p>Puesto Ocupado / Buscado</p>
+            </div>
+        </header>
 
-        <input type="submit" name="registro">
-        <input type="reset" >
-
-    </form>
+        <div class="content">
+            <div class="sidebar">
+                <section class="contacto">
+                    <h2>CONTACTO</h2>
+                    <form action="index.php" name="formulario" method="post">
+                        
+                        <input type="text" name="telefono" placeholder="telefono">
+                        <input type="email" name="correo" placeholder="correo">
+                        
+                        
+                        <input type="text" name="idiomas" placeholder="idiomas">
+                        <input type="text" name="habilidades" placeholder="habilidades">
+                        <input type="submit" name="registro" value="Registrar">
+                        <input type="reset">
+                    </form>
+                </section>
+            </div>
+            <div class="main-content">
+                <section class="experiencia">
+                    <h2>EXPERIENCIA LABORAL</h2>
+                    <input type="text" name="experiencia_laboral" placeholder="experiencia laboral">
+                </section>
+                <section class="formacion">
+                    <h2>FORMACIÓN</h2>
+                    <input type="text" name="formacion" placeholder="formación">
+                </section>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
+
 
 <?php
     if(isset($_POST['registro'])){
